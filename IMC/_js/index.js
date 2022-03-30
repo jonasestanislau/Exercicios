00335a2604eafb05bbcd2574b1503aa29e481327
jonasestanislau <jonas.redes21@gmail.com>
2022-03-30 08:37:47 -0300

    Update

---
+++ IMC/_js/index.js
@@ -0,0 +1,13 @@
+// Selecionando os elementos HTML
+let campoResultado = document.getElementById("campoResultado");
+let campoPeso = document.getElementById("campoPeso");
+let campoAltura = document.getElementById("campoAltura");
+let botaoCalcular = document.getElementById("botaoCalcular");
+
+// Evento para chamar a função que irá calcular o IMC
+botaoCalcular.addEventListener('click',calculoIMC);
+
+// Função que calcular o IMC
+function calculoIMC() {
+    
+}
