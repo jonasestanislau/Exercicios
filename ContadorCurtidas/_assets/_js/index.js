3e674bd7e0ac385ebefe06c477e801cb11c5d287
jonasestanislau <jonas.redes21@gmail.com>
2022-04-26 07:56:53 -0300

    Arquivo
    Ajustes

--- ContadorCurtidas/_assets/_js/index.js
+++ ContadorCurtidas/_assets/_js/index.js
@@ -4,19 +4,19 @@ let contador = 0;// variável que armazena o total de curtidas
 
 
 btnCurtir.forEach((item) => {
-    let stateClick = true;// variável que armazena o valor de true ou false para o quando ocorrer o click
+    let stateClick = false;// variável que armazena o valor de true ou false para o quando ocorrer o click
     item.children[1].addEventListener('click', () => {
         //condição para alterar o valor da variável stateClick e alterar o estilo dos elementos mostrados na tela
-        if(stateClick === true) {
+        if(stateClick != true) {
             totalCurtidas.textContent = ++contador;
             item.children[0].textContent = 1;
             item.children[1].style.color = '#09c';
-            stateClick = false;
+            stateClick = true;
         }else {
             totalCurtidas.textContent = --contador;
             item.children[0].textContent = 0;
             item.children[1].style.color = '#000';
-            stateClick = true;
+            stateClick = false;
         }
     })
 })
